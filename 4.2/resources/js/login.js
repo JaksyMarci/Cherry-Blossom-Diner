c8358927e0979d0a20a4ec2b06b5d9455cdfbf56
Levi <e8gqnb@inf.elte.hu>
2022-10-21 10:54:13 +0200

    Added partial login front-end

---
+++ 4.2/resources/js/login.js
@@ -0,0 +1,7 @@
+//require('./bootstrap'); "Not defined"
+
+import {createApp} from 'vue'
+
+import App from './components/login.vue'
+
+createApp(App).mount("#login")
--- 4.2/webpack.config.js
+++ 4.2/webpack.config.js
@@ -15,9 +15,9 @@ module.exports = async () => {
 
     return mix.build();
 };
-
+/*
 mix.js('resources/js/app.js', 'public/js')
     .vue() // <- Add this
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ]);*/
