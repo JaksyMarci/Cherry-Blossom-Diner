2dead5819d6207fc683a5ab54c48d2506584d6b9
Balazs Stavniczki <y6pcu7@inf.elte.hu>
2022-12-01 15:57:06 +0100

    added scribe documentation creator

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,38 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::middleware('auth')->group(function () {
+    /**
+     * *HOME PAGE*
+     * only authorized user can see it.
+     * after login you end up here.
+     * All the tables are visible. You can select the menu by clicking a table
+     */
     Route::get('/home', function () {
         return redirect()->route('tables.index');
     })->name('tables');
 
+    /**
+     * *LOGIN PAGE*
+     * Every other page is reached via login.
+     * Every waiter/waitress has their email and password combo which they can use to authenticate.
+     */
     Route::get('/login', function () {
         return redirect()->route('tables.index');
     })->name('tables');
+
+    /**
+     * *DOCUMENTATION PAGE*
+     * only the admin user can see it.
+     * It's our documentation page about our API's.
+     * Every API endpoint is documented here.
+     */
+    Route::get('/documentation', function () {
+        if (Auth::user()->is_admin) {
+            return view('./scribe/index');
+        } else {
+            return redirect()->route('tables.index');
+        }
+    })->name('documentation');
 
     Route::get('/', function () {
         return redirect()->route('tables.index');
